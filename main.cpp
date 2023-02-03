b458840c803a9e3dc1a03b05da2865d4ff109644
nassimoyah <107773569+nassimoyah@users.noreply.github.com>
2023-02-03 17:29:21 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#define sensorPin A0
+int sensorValue = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  if (sensorValue > 300 ) {
+    Serial.println(" gaz detected ");
+    // Trigger an alert here, for example, by turning on an LED.
+  }
+  else {
+    Serial.println("no gaz detected");
+  }
+  delay(1000);
+}
